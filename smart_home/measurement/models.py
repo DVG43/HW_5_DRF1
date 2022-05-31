5f4c1b570433911758af846d793616f5628f45f8
Galuta Dmitrii <dvgaluta@gmail.com>
2022-06-01 00:42:19 +0300

    Commit_3

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    desccription = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+
+class Measurement(models.Model):
+    temperature = models.FloatField()
+    creating = models.DateTimeField()
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measures')
+
+
